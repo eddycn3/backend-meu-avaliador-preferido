efe89d867f968fd8a545ca6e6a0ec37b81a367a7
eduardo c neto <eddycn3@gmail.com>
2020-05-27 17:43:34 -0300

    .

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ var cors_1 = __importDefault(require("cors"));
 var routes_1 = __importDefault(require("./routes"));
 var errorHandlerMiddleware_1 = __importDefault(require("./middlewares/errorHandlerMiddleware"));
 var app = express_1.default();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT);
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default.routes);
