3c34353c571b8dca8f7b3f376ef055a0ff41105c
eduardo c neto <eddycn3@gmail.com>
2020-05-29 18:56:40 -0300

    .

--- dist/models/common/AuthModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AuthModel {
-}
-exports.AuthModel = AuthModel;
-//# sourceMappingURL=AuthModel.js.map
--- dist/utils/enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserType;
-(function (UserType) {
-    UserType[UserType["Avalidor"] = 1] = "Avalidor";
-    UserType[UserType["Aluno"] = 2] = "Aluno";
-})(UserType = exports.UserType || (exports.UserType = {}));
-//# sourceMappingURL=enums.js.map
